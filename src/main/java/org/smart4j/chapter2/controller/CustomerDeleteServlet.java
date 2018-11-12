8911ed3b95d365ef74a556997bf7e844b8e75465
lilimin <limin.li@pingcl.com>
2018-11-12 18:10:03 +0800

    pick2

--- src/main/java/org/smart4j/chapter2/controller/CustomerDeleteServlet.java
+++ src/main/java/org/smart4j/chapter2/controller/CustomerDeleteServlet.java
@@ -4,4 +4,11 @@ package org.smart4j.chapter2.controller;
  * Created by Administrator on 2017/8/30.
  */
 public class CustomerDeleteServlet {
+     @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        DateFormat dft = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = dft.format(new Date());
+        req.setAttribute("currentTime", currentTime);
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req, resp);
+    }
 }
